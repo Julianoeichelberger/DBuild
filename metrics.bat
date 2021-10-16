5ded86e63298ca6718ac2f22773e4a6fb043c81c
Juliano Eichelberger <juliano.eichelberger@zonesoft.org>
2021-10-16 20:30:32 +0100

    remove file

--- metrics.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Program Files (x86)\Embarcadero\Studio\20.0\"
-AuditsCLI.exe --metrics --html -o "C:\Users\USER\Documents\zs-new\bin\output\html\FrontOffice.html" -u "%WORKSPACE%/src/projects/FrontOffice/" "C:\Users\USER\Documents\zs-new\src\projects\FrontOffice\FrontOffice.dproj" 
